ba3921a3da1071d60331f5480ae4967bfc30d9d4
NaiskDeV <54291577+NaiskDeV@users.noreply.github.com>
2019-11-14 18:01:30 +0100

    Create Terrain.java

---
+++ src/towers/Terrain.java
@@ -0,0 +1,28 @@
+package towers;
+
+public class Terrain {
+
+	public static void main(String[] args) {
+		
+		Ennemi grosMechant = new Ennemi(100,2);
+		Ennemi unAutreEnnemi = new Ennemi(50,4);
+		
+		grosMechant.affiche();
+		unAutreEnnemi.affiche();
+		
+		grosMechant.seDeplace(5, 8);
+		
+		grosMechant.affiche();
+		
+		grosMechant.recevoirDegats(50);
+		unAutreEnnemi.recevoirDegats(10);
+		
+		grosMechant.affiche();
+		unAutreEnnemi.affiche();
+		
+		System.out.println(grosMechant);
+		grosMechant.toString();
+
+	}
+
+}
