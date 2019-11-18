c4ca447214bf9c3dcbb2e30a8305740b20ac8fe6
NaiskDeV <54291577+NaiskDeV@users.noreply.github.com>
2019-11-18 16:56:12 +0100

    Create C_POINT.java

---
+++ src/tp2/C_POINT.java
@@ -0,0 +1,29 @@
+package tp2_poo;
+
+public class C_POINT extends C_POSITION {
+
+	char nom;
+	
+	//Constructeur par défaut
+	public C_POINT() {
+		super(0, 0);
+	}
+	
+	//Constructeur d'initialisation
+	public C_POINT(int px, int py, char nom) {
+		super(px, py);
+		this.nom = nom;
+	}
+	
+	//Constructeur de recopie
+	public C_POINT (C_POINT point) {
+		super(point.x, point.y);
+		nom = point.nom;
+	}
+	
+	public void aff() {
+		System.out.println(nom+"("+x+","+y+")");
+	}
+	
+	
+}
