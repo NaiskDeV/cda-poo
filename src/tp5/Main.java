bf4baaad36024ba9b58614c28de16bdd2b59f70b
NaiskDeV <54291577+NaiskDeV@users.noreply.github.com>
2019-11-18 22:31:35 +0100

    Create Main.java

---
+++ src/tp5/Main.java
@@ -0,0 +1,22 @@
+package tp5_poo;
+
+public class Main {
+	
+	Payant ligne_prix[] = new Payant [6];
+	
+	Service service1 = new Service(10);
+	Service service2 = new Service(20);
+	Service service3 = new Service(30);
+	
+	Boisson boisson1 = new Boisson(1);
+	Boisson boisson2 = new Boisson(2);
+	Boisson boisson3 = new Boisson(3);
+	
+	ligne_prix[0] = service1;
+	ligne_prix[1] = service2;
+	ligne_prix[2] = service3;
+	ligne_prix[3] = boisson1;
+	ligne_prix[4] = boisson2;
+	ligne_prix[5] = boisson3;
+	
+}
