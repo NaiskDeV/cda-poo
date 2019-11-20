0d868eb4af498e7389222d6b63c563dca7d735b1
LAU.LECHAIX <LAU.LECHAIX@s124-15.pedago.mermoz.edu>
2019-11-20 14:19:50 +0100

    Exo1

--- src/tp5/Main.java
+++ src/tp5/Main.java
@@ -2,21 +2,35 @@ package tp5;
 
 public class Main {
 	
-	Payant ligne_prix[] = new Payant [6];
+	public static void main(String[] args) {
+		
+		//Exo1
+		Service service1 = new Service(10);
+		Service service2 = new Service(20);
+		Service service3 = new Service(30);
+		
+		Boisson boisson1 = new Boisson(1);
+		Boisson boisson2 = new Boisson(2);
+		Boisson boisson3 = new Boisson(3);
+		
+		Payant ligne_prix[] = new Payant [6];
+		ligne_prix[0] = service1;
+		ligne_prix[1] = service2;
+		ligne_prix[2] = service3;
+		ligne_prix[3] = boisson1;
+		ligne_prix[4] = boisson2;
+		ligne_prix[5] = boisson3;
 	
-	Service service1 = new Service(10);
-	Service service2 = new Service(20);
-	Service service3 = new Service(30);
+		float somme = 0;
+		for (int i = 0; i < ligne_prix.length ; i++){
+	        somme += ligne_prix[i].getPrix();
+	    }
+	    
+	    System.out.println("Somme des prix : "+somme);
+
+	}
 	
-	Boisson boisson1 = new Boisson(1);
-	Boisson boisson2 = new Boisson(2);
-	Boisson boisson3 = new Boisson(3);
 	
-	ligne_prix[0] = service1;
-	ligne_prix[1] = service2;
-	ligne_prix[2] = service3;
-	ligne_prix[3] = boisson1;
-	ligne_prix[4] = boisson2;
-	ligne_prix[5] = boisson3;
 	
+    
 }
