2452397afe04158c823c750f1517bd287b8dbf37
NaiskDeV <54291577+NaiskDeV@users.noreply.github.com>
2019-11-20 10:55:26 +0100

    Create Carte.java

---
+++ src/bataille/Carte.java
@@ -0,0 +1,25 @@
+package bataille;
+
+public class Carte {
+	String valeur;
+	String couleur;
+	
+	
+	public Carte (String valeur, String couleur) {
+		this.valeur = valeur;
+		this.couleur = couleur;
+	}
+	
+	public void superieurA() {
+		
+	}
+	
+	public void memeValeur() {
+		
+	}
+	
+	public String toString() {
+		return "test";
+	}
+	
+	}
