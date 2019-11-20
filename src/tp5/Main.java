b9cb724b6af2942f571975240d91043387e8125a
LAU.LECHAIX <LAU.LECHAIX@s124-15.pedago.mermoz.edu>
2019-11-20 16:44:44 +0100

    tp5

--- src/tp5/Main.java
+++ src/tp5/Main.java
@@ -2,6 +2,7 @@ package tp5;
 
 public class Main {
 	
+	
 	public static void main(String[] args) {
 		
 		//Exo1
@@ -28,6 +29,10 @@ public class Main {
 	    
 	    System.out.println("Somme des prix : "+somme);
 
+	    
+	    //Exo 2
+	    
+	
 	}
 	
 	
